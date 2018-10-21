ba8964e5aecb09bcbe4a7415b4a6e63749090274
KamilCzerniak <kamil.z.czerniak@gmail.com>
2018-10-21 19:42:20 +0200

    Final version

--- BanknotesAndCoins/src/BanknotesAndCoins.java
+++ BanknotesAndCoins/src/BanknotesAndCoins.java
@@ -4,11 +4,11 @@ import java.util.List;
 public class BanknotesAndCoins {
 
     public static final int[] NOMINAL_ZLOTY = {200, 100, 50, 20, 10, 5, 2, 1};
-public static final int[] NOMINAL_GROSZ = {50, 10, 2, 1};
+    public static final int[] NOMINAL_GROSZ = {50, 10, 2, 1};
 
     public static String money(int zloty, int grosz) {
         List<String> list = new ArrayList<>();
-        for (int nom :NOMINAL_ZLOTY ) {
+        for (int nom : NOMINAL_ZLOTY) {
             if (zloty / nom != 0) {
                 list.add(zloty / nom + " x " + nom + " zł");
                 zloty %= nom;
